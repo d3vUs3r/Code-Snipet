3b5c6f902e9d8a43891fbeefdcab9011fd60e9d1
d3vUs3r <115107997+d3vUs3r@users.noreply.github.com>
2023-06-25 23:53:42 +0100

    Reflective DLL Injection

--- Windows_DLL_Injection_v2.cpp
+++ Windows_DLL_Injection_v2.cpp
@@ -1,6 +1,18 @@
 #include <Windows.h>
 #include <iostream>
 
+typedef struct BASE_RELOCATION_BLOCK {
+	DWORD PageAddress;
+	DWORD BlockSize;
+} BASE_RELOCATION_BLOCK, * PBASE_RELOCATION_BLOCK;
+
+typedef struct BASE_RELOCATION_ENTRY {
+	USHORT Offset : 12;
+	USHORT Type : 4;
+} BASE_RELOCATION_ENTRY, * PBASE_RELOCATION_ENTRY;
+
+using DLLEntry = BOOL(WINAPI*)(HINSTANCE dll, DWORD reason, LPVOID reserved);
+
 int main(int argc, char* argv[])
 {
 	DWORD processId = GetCurrentProcessId();
